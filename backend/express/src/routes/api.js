90260fcdecffb41c0f31508b36b9032d8aa908e1
JavierTomasTormo <106741000+JavierTomasTormo@users.noreply.github.com>
2025-01-17 15:50:39 +0100

    Docker y Live Reload
    express/Dockerfile, next/Dockerfile,, docker-compose,

--- backend/express/src/routes/api.js
+++ backend/express/src/routes/api.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 // Define your routes here
 router.get("/", (req, res) => {
-    res.send("API is working");
+    res.json({ message: "Welcome to the API!" });
 });
 
 module.exports = router;
