a1384cd9b6da0f4e9d2923d7061f2c44be8649d4
JavierTomasTormo <106741000+JavierTomasTormo@users.noreply.github.com>
2025-01-21 20:27:39 +0100

    Model, Relationships Activities

---
+++ backend/django/apps/activities/urls.py
@@ -0,0 +1,10 @@
+from rest_framework.routers import DefaultRouter
+from django.urls import path, include
+from .views import ActivityViewSet
+
+router = DefaultRouter()
+router.register(r'activities', ActivityViewSet)
+
+urlpatterns = [
+    path('', include(router.urls)),
+]
