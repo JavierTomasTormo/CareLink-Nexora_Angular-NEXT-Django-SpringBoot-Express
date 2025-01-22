c0e0f2ffb47b8a9489909263ec0d624d3875104d
JavierTomasTormo <106741000+JavierTomasTormo@users.noreply.github.com>
2025-01-22 19:43:55 +0100

    Corrección KeySensitive Django --> DB

--- backend/django/vitalnest/token/blacklist/models.py
+++ backend/django/vitalnest/token/blacklist/models.py
@@ -5,11 +5,13 @@ class BlackList(models.Model):
     id_user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='blacklist', db_column='id_user', blank=True, null=True)
     email = models.EmailField(max_length=255)
     token_expired = models.CharField(max_length=255)
+    # createdat = models.DateTimeField(auto_now_add=True)
+    # updatedat = models.DateTimeField(auto_now=True)
     createdat = models.DateTimeField(auto_now_add=True)
-    updatedat = models.DateTimeField(auto_now=True)
+    updatedat = models.DateTimeField(auto_now=True) 
 
     class Meta:
-        ordering = ['-createdAt']
+        ordering = ['-createdat']
         db_table = 'blacklist'
 
     def __str__(self):
