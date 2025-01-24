d7f8de1aa4fa041052e5ff8212ce8ab52e1dada3
JavierTomasTormo <106741000+JavierTomasTormo@users.noreply.github.com>
2025-01-24 11:02:56 +0100

    Enpoints --> ImagesActivities

--- backend/django/core/urls.py
+++ backend/django/core/urls.py
@@ -24,4 +24,5 @@ urlpatterns = [
     path('api/', include('vitalnest.usertype.userpatient.urls')),
     path('api/', include('vitalnest.food.meals.urls')),
     path('api/', include('vitalnest.images.images.urls')),
+    path('api/', include('vitalnest.images.imagesactivities.urls')),
 ]
