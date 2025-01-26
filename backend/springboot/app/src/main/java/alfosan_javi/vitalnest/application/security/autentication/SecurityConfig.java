9bd88669ee9332886292c1f1e748fd134803f88f
Llorenç Alfonso Sanchis <163330679+alfosan@users.noreply.github.com>
2025-01-26 17:01:10 +0100

    Created - SpringBoot --> Security

---
+++ backend/springboot/app/src/main/java/alfosan_javi/vitalnest/application/security/autentication/SecurityConfig.java
@@ -0,0 +1,40 @@
+package alfosan_javi.vitalnest.application.security.autentication;
+
+import alfosan_javi.vitalnest.application.security.jwt.JwtAuthenticationFilter;
+import alfosan_javi.vitalnest.application.security.jwt.JwtUtils;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.argon2.Argon2PasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    private final JwtUtils jwtUtils;
+
+    public SecurityConfig(JwtUtils jwtUtils) {
+        this.jwtUtils = jwtUtils;
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new Argon2PasswordEncoder(16, 32, 1, 65536, 3);
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.csrf(csrf -> csrf.disable())
+            .authorizeHttpRequests(auth -> auth
+                .requestMatchers("/payments/**", "/inscriptions/**").authenticated()
+                .anyRequest().permitAll()
+            )
+            .addFilterBefore(new JwtAuthenticationFilter(jwtUtils), UsernamePasswordAuthenticationFilter.class);
+
+        return http.build();
+    }
+}
