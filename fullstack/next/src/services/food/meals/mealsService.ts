5924c1296445dba273c5a693fdeaf4e2b0da28b0
JavierTomasTormo <106741000+JavierTomasTormo@users.noreply.github.com>
2025-01-28 17:17:35 +0100

    BedRooms--> Correccion Action_Servers --> Services

--- fullstack/next/src/services/food/meals/mealsService.ts
+++ fullstack/next/src/services/food/meals/mealsService.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-import { MEALS_API_URL } from '@/store/Constants';
+import { MEALS_API_URL, MealData } from '@/store/Constants';
 
 
 export const fetchAllMeals = async () => {
@@ -20,6 +20,28 @@ export const fetchMealById = async (id: number) => {
         return response.data;
     } catch (error) {
         console.error(`Error fetching meal with id ${id}:`, error);
+        throw error;
+    }
+};
+
+
+
+export const createMeal = async (mealData: MealData) => {
+    try {
+        const response = await axios.post(MEALS_API_URL, mealData);
+        return response.data;
+    } catch (error) {
+        console.error('Error creating meal:', error);
+        throw error;
+    }
+};
+
+export const updateMeal = async (id: number, mealData: MealData) => {
+    try {
+        const response = await axios.put(`${MEALS_API_URL}${id}/`, mealData);
+        return response.data;
+    } catch (error) {
+        console.error(`Error updating meal with id ${id}:`, error);
         throw error;
     }
 };
