8ae055512182bb00891357f5c81b1f530ddf159f
JavierTomasTormo <106741000+JavierTomasTormo@users.noreply.github.com>
2025-01-28 17:17:58 +0100

    Rooms--> Correccion Action_Servers --> Services

--- fullstack/next/src/services/rooms/room/roomService.ts
+++ fullstack/next/src/services/rooms/room/roomService.ts
@@ -10,7 +10,7 @@ export const fetchAllRooms = async () => {
 
         const roomsWithImages = await Promise.all(
             rooms.map(async (room: RoomData) => {
-                const imagesResponse = await axios.get(`${IMAGES_ROOMS_API_URL}?num_room=${room.num_room}`);
+                const imagesResponse = await axios.get(`${IMAGES_ROOMS_API_URL}?id_room=${room.id_room}`);
                 return { ...room, images: imagesResponse.data };
             })
         );
@@ -30,5 +30,36 @@ export const fetchRoomById = async (id: number) => {
   } catch (error) {
     console.error(`Error fetching room with id ${id}:`, error);
     throw error;
+  }
+};
+
+
+export const createRoom = async (roomData: RoomData) => {
+  try {
+      const response = await axios.post(ROOMS_API_URL, roomData);
+      return response.data;
+  } catch (error) {
+      console.error('Error creating room:', error);
+      throw error;
+  }
+};
+
+export const updateRoom = async (id: number, roomData: RoomData) => {
+  try {
+      const response = await axios.put(`${ROOMS_API_URL}${id}/`, roomData);
+      return response.data;
+  } catch (error) {
+      console.error(`Error updating room with id ${id}:`, error);
+      throw error;
+  }
+};
+
+export const deleteRoom = async (id: number) => {
+  try {
+      await axios.delete(`${ROOMS_API_URL}${id}/`);
+      await axios.delete(`${IMAGES_ROOMS_API_URL}?id_room=${id}`);
+  } catch (error) {
+      console.error(`Error deleting room with id ${id}:`, error);
+      throw error;
   }
 };
