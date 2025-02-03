77d44356d829c84555f23fac7316efd1e9e3b094
JavierTomasTormo <106741000+JavierTomasTormo@users.noreply.github.com>
2025-02-03 19:12:24 +0100

    Django --> Apply --> Tutor_Role -->User Tutor

--- backend/django/core/urls.py
+++ backend/django/core/urls.py
@@ -19,20 +19,33 @@ from django.urls import path, include
 
 urlpatterns = [
     # path('admin/', admin.site.urls),
-    path('api/', include('vitalnest.activities.urls')),
-    path('api/', include('vitalnest.usertype.user.urls')),
-    path('api/', include('vitalnest.usertype.userpatient.urls')),
+
+    # Activities ####################
+        path('api/', include('vitalnest.activities.urls')),
+
+    # Users ####################
+        path('api/', include('vitalnest.usertype.user.urls')),
+        path('api/', include('vitalnest.usertype.userpatient.urls')),
+
+    # Meals ####################
     path('api/', include('vitalnest.food.meals.urls')),
-    path('api/', include('vitalnest.images.images.urls')),
-    path('api/', include('vitalnest.images.imagesactivities.urls')),
-    path('api/', include('vitalnest.images.imagesrooms.urls')),
-    path('api/', include('vitalnest.rooms.room.urls')),
-    path('api/', include('vitalnest.rooms.bedroom.urls')),
-    path('api/', include('vitalnest.timetables.day.urls')),
-    path('api/', include('vitalnest.timetables.daysofweek.urls')),
-    path('api/', include('vitalnest.timetables.hour.urls')),
-    path('api/', include('vitalnest.timetables.month.urls')),
-    path('api/', include('vitalnest.timetables.year.urls')),
+
+    # Images ####################
+        path('api/', include('vitalnest.images.images.urls')),
+        path('api/', include('vitalnest.images.imagesactivities.urls')),
+        path('api/', include('vitalnest.images.imagesrooms.urls')),
+
+    # Rooms ####################
+        path('api/', include('vitalnest.rooms.room.urls')),
+        path('api/', include('vitalnest.rooms.bedroom.urls')),
+
+    # TimeTables ####################
+        path('api/', include('vitalnest.timetables.day.urls')),
+        path('api/', include('vitalnest.timetables.daysofweek.urls')),
+        path('api/', include('vitalnest.timetables.hour.urls')),
+        path('api/', include('vitalnest.timetables.month.urls')),
+        path('api/', include('vitalnest.timetables.year.urls')),
+
     # AUTH #####################
-    path('api/', include('vitalnest.auth.tutor.register.urls')),
+        path('api/', include('vitalnest.auth.tutor.register.urls')),
 ]
--- backend/django/vitalnest/auth/tutor/register/views.py
+++ backend/django/vitalnest/auth/tutor/register/views.py
@@ -1,10 +1,10 @@
-
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 from django.utils.decorators import method_decorator
 from django.views import View
 from django.contrib.auth.hashers import make_password
 from vitalnest.usertype.user.models import User
+from vitalnest.roles.userrole.models import UserRole
 import json
 
 @method_decorator(csrf_exempt, name='dispatch')
@@ -22,6 +22,10 @@ class RegisterTutorView(View):
                 profile_img=data.get('profile_img', '')
             )
             user.save()
+
+            user_role = UserRole(id_user=user, id_role_id=3)
+            user_role.save()
+
             return JsonResponse({'status': 'success', 'message': 'User registered successfully'}, status=201)
         except Exception as e:
             return JsonResponse({'status': 'error', 'message': str(e)}, status=400)
