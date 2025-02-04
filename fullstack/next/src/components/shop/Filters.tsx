71ccdd32bcd6a0213585fb0ddd2b286c620ba42d
Llorenç Alfonso Sanchis <163330679+alfosan@users.noreply.github.com>
2025-02-04 19:37:22 +0100

    Activities --> Next --> Slide, List and Filters.

---
+++ fullstack/next/src/components/shop/Filters.tsx
@@ -0,0 +1,79 @@
+"use client";
+
+import { useState, useEffect } from "react";
+import { useRouter, useSearchParams } from "next/navigation";
+import { useDispatch } from 'react-redux';  // Importar useDispatch
+import styles from "@/styles/meals/Filters.module.css";
+import { filterActivitiesByType } from "@/store/slices/activitiesSlice";
+
+const Filters = ({ onFilterChange }: { onFilterChange: (id: number, color: string) => void }) => {
+  const router = useRouter();
+  const searchParams = useSearchParams();
+  const dispatch = useDispatch(); 
+
+  const filterColors = [
+    { id: 1, color: "#FFE5D9", name: "Cuidados" },
+    { id: 2, color: "#E3F4D7", name: "Exteriores" },
+    { id: 3, color: "#FFE0E9", name: "Rehabilitaciónes" },
+    { id: 4, color: "#E0F4FF", name: "Relajación" },
+    { id: 5, color: "#E8E0FF", name: "Educación" },
+  ];
+
+  const [activeFilter, setActiveFilter] = useState<number | null>(null);
+  const [activeFilterColor, setActiveFilterColor] = useState<string>("");
+
+  // Actualizar el filtro al cargar el componente o cambiar la URL
+  useEffect(() => {
+    const queryFilter = searchParams.get("activity_type");
+    if (queryFilter) {
+      const filterId = parseInt(queryFilter, 10);
+      const filter = filterColors.find((f) => f.id === filterId);
+      if (filter) {
+        setActiveFilter(filterId);
+        setActiveFilterColor(filter.color);
+        onFilterChange(filterId, filter.color);
+        document.body.setAttribute('data-sld', (filterId - 1).toString());
+        // Despachar acción para filtrar las actividades por tipo
+        dispatch(filterActivitiesByType(filterId));
+      }
+    }
+  }, [searchParams, dispatch, onFilterChange]); // Asegúrate de que dispatch esté como dependencia
+
+  // Manejar el clic en los filtros
+  const handleFilterClick = (id: number, color: string) => {
+    setActiveFilter(id);
+    setActiveFilterColor(color);
+    onFilterChange(id, color);
+    router.push(`?activity_type=${id}`, { scroll: false });
+    document.body.setAttribute('data-sld', (id - 1).toString());
+    // Despachar acción para filtrar las actividades por tipo
+    dispatch(filterActivitiesByType(id));
+  };
+
+  return (
+    <header
+      className={styles.header}
+      style={{
+        background: activeFilterColor,
+      }}
+    >
+      <div className={styles.logo}>Filtros</div>
+      <div className={styles.headerMenu}>
+        {filterColors.map((filter) => (
+          <span
+            key={filter.id}
+            className={styles.menuItem}
+            onClick={() => handleFilterClick(filter.id, filter.color)}
+            style={{
+              backgroundColor: activeFilter === filter.id ? filter.color : "",
+            }}
+          >
+            {filter.name}
+          </span>
+        ))}
+      </div>
+    </header>
+  );
+};
+
+export default Filters;
