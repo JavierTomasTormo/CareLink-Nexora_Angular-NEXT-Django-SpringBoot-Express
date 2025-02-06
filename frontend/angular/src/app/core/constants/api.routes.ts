6b312b49471d8574b5386fc9777559c65b070501
JavierTomasTormo <106741000+JavierTomasTormo@users.noreply.github.com>
2025-02-06 16:30:39 +0100

    Models --> Angular --> LoginModel --> UserTutor

--- frontend/angular/src/app/core/constants/api.routes.ts
+++ frontend/angular/src/app/core/constants/api.routes.ts
@@ -1,9 +1,9 @@
 import { environment } from '../../../environments/environment';
 
 export const API_ROUTES = {
-  AUTH: {
-    LOGIN: `${environment.apiUrl}/auth/tutor/login`,
-    REGISTER: `${environment.apiUrl}/auth/tutor/register`,
-    LOGOUT: `${environment.apiUrl}/auth/tutor/logout`
-  }
+    AUTH: {
+        LOGIN: `${environment.apiUrl}/auth/tutor/login`,
+        REGISTER: `${environment.apiUrl}/auth/tutor/register`,
+        LOGOUT: `${environment.apiUrl}/auth/tutor/logout`
+    }
 };
