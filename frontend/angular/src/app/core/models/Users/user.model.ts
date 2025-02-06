ac57800534dc0ee65e090debf836cc9e8a5f5066
JavierTomasTormo <106741000+JavierTomasTormo@users.noreply.github.com>
2025-02-06 16:31:06 +0100

    Model --> Angular --> Register --> Request & Response

---
+++ frontend/angular/src/app/core/models/Users/user.model.ts
@@ -0,0 +1,11 @@
+export interface User {
+    id_user: number;
+    email: string;
+    name: string;
+    isactive: number;
+    createdat: string;
+    updatedat: string;
+    phone_number: string;
+    address: string;
+    profile_img: string;
+}
