4f34adae0a9445846fec40651d4335a3b60b372c
JavierTomasTormo <106741000+JavierTomasTormo@users.noreply.github.com>
2025-02-06 15:58:08 +0100

    Apply --> Angular --> Loading Interceptor

--- frontend/angular/src/main.ts
+++ frontend/angular/src/main.ts
@@ -1,6 +1,17 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import { provideRouter } from '@angular/router';
+import { routes } from './app/app.routes';
+import { HTTP_INTERCEPTORS } from '@angular/common/http';
+import { LoadingInterceptor } from './app/core/interceptors/utils/loading.interceptor';
 
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+bootstrapApplication(AppComponent, {
+  providers: [
+    provideRouter(routes),
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: LoadingInterceptor,
+      multi: true
+    }
+  ]
+}).catch(err => console.error(err));
