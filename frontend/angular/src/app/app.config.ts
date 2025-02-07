c4d7d43f7de8508277e2740d56d99707f28b805b
JavierTomasTormo <106741000+JavierTomasTormo@users.noreply.github.com>
2025-02-07 15:42:41 +0100

    Fix --> Angular --> Interceptors --> Auth

--- frontend/angular/src/app/app.config.ts
+++ frontend/angular/src/app/app.config.ts
@@ -2,20 +2,14 @@ import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
-import { AuthInterceptor } from './core/interceptors/auth.interceptor';
-import { LoadingInterceptor } from './core/interceptors/utils/loading.interceptor';
-import { HTTP_INTERCEPTORS } from '@angular/common/http';
+import { authInterceptor } from './core/interceptors/auth/auth.interceptor';
+import { loadingInterceptor } from './core/interceptors/utils/loading.interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
     provideHttpClient(
-      withInterceptors([AuthInterceptor])
-    ),
-    {
-      provide: HTTP_INTERCEPTORS,
-      useClass: LoadingInterceptor,
-      multi: true
-    }
+      withInterceptors([authInterceptor, loadingInterceptor])
+    )
   ]
 };
