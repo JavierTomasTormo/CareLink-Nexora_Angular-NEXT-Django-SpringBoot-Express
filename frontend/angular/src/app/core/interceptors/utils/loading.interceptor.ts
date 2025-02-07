922faf196be46cbab71684a72ec0badc69e9280c
JavierTomasTormo <106741000+JavierTomasTormo@users.noreply.github.com>
2025-02-07 15:42:36 +0100

    Fix --> Angular --> Interceptors --> Loading

--- frontend/angular/src/app/core/interceptors/utils/loading.interceptor.ts
+++ frontend/angular/src/app/core/interceptors/utils/loading.interceptor.ts
@@ -1,17 +1,31 @@
-import { Injectable } from '@angular/core';
-import { HttpInterceptor, HttpRequest, HttpHandler, HttpEvent } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { HttpInterceptorFn } from '@angular/common/http';
+import { inject } from '@angular/core';
 import { finalize } from 'rxjs/operators';
 import { LoadingService } from '../../services/utils/loading.service';
 
-@Injectable()
-export class LoadingInterceptor implements HttpInterceptor {
-  constructor(private loadingService: LoadingService) {}
+export const loadingInterceptor: HttpInterceptorFn = (request, next) => {
+  const loadingService = inject(LoadingService);
+  
+  loadingService.show();
+  
+  return next(request).pipe(
+    finalize(() => loadingService.hide())
+  );
+};
+// import { Injectable } from '@angular/core';
+// import { HttpInterceptor, HttpRequest, HttpHandler, HttpEvent } from '@angular/common/http';
+// import { Observable } from 'rxjs';
+// import { finalize } from 'rxjs/operators';
+// import { LoadingService } from '../../services/utils/loading.service';
 
-  intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-    this.loadingService.show();
-    return next.handle(request).pipe(
-      finalize(() => this.loadingService.hide())
-    );
-  }
-}
+// @Injectable()
+// export class LoadingInterceptor implements HttpInterceptor {
+//   constructor(private loadingService: LoadingService) {}
+
+//   intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+//     this.loadingService.show();
+//     return next.handle(request).pipe(
+//       finalize(() => this.loadingService.hide())
+//     );
+//   }
+// }
