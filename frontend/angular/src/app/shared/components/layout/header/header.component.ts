e33e723f5759634f47a4e2cd9ff6e737ac2ea49f
JavierTomasTormo <106741000+JavierTomasTormo@users.noreply.github.com>
2025-02-08 12:48:26 +0100

    Add --> Angular --> SharedRoutes

--- frontend/angular/src/app/shared/components/layout/header/header.component.ts
+++ frontend/angular/src/app/shared/components/layout/header/header.component.ts
@@ -1,17 +1,51 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { TokenService } from '../../../../core/services/token/token.service';
+import { UserService } from '../../../../core/services/auth/user.service';
+import { SHARED_ROUTES } from '../../../../core/constants/shared.routes';
 
 @Component({
     selector: 'app-header',
     standalone: true,
-    imports: [RouterModule],
+    imports: [RouterModule, CommonModule],
     templateUrl: './header.component.html',
     styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent {
     menuOpen = false;
+    routes = SHARED_ROUTES;
+    
+    constructor(
+        public tokenService: TokenService,
+        private userService: UserService,
+    ) {}
     
     toggleMenu() {
         this.menuOpen = !this.menuOpen;
     }
+
+    logout() {
+        this.userService.logout();
+        this.tokenService.clearAll();
+        window.location.href = this.routes.NEXT.HOME;
+    }
 }
+
+// import { Component } from '@angular/core';
+// import { RouterModule } from '@angular/router';
+
+// @Component({
+//     selector: 'app-header',
+//     standalone: true,
+//     imports: [RouterModule],
+//     templateUrl: './header.component.html',
+//     styleUrls: ['./header.component.scss']
+// })
+// export class HeaderComponent {
+//     menuOpen = false;
+    
+//     toggleMenu() {
+//         this.menuOpen = !this.menuOpen;
+//     }
+// }
