71a32fdf3e0a2477f88a924fdcb2a83b77759d5d
JavierTomasTormo <106741000+JavierTomasTormo@users.noreply.github.com>
2025-02-08 12:18:35 +0100

    Services --> Angular --> Token Service Updated

--- frontend/angular/src/app/core/services/token/token.service.ts
+++ frontend/angular/src/app/core/services/token/token.service.ts
@@ -1,28 +1,42 @@
 import { Injectable } from '@angular/core';
+import { User } from '../../models/Users/user.model';
+import { TOKEN_ROUTES } from '../../constants/token.routes';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class TokenService {
+
   setTokens(accessToken: string, refreshToken: string): void {
-    localStorage.setItem('accessToken', accessToken);
-    localStorage.setItem('refreshToken', refreshToken);
+    localStorage.setItem(TOKEN_ROUTES.TOKEN_USER.TOKEN_KEY, accessToken);
+    localStorage.setItem(TOKEN_ROUTES.TOKEN_USER.REFRESH_TOKEN_KEY, refreshToken);
   }
 
-  clearTokens(): void {
-    localStorage.removeItem('accessToken');
-    localStorage.removeItem('refreshToken');
+  setUserInfo(user: User): void {
+    localStorage.setItem(TOKEN_ROUTES.TOKEN_USER.USER_KEY, JSON.stringify(user));
+  }
+
+  getUserInfo(): User | null {
+    const userStr = localStorage.getItem(TOKEN_ROUTES.TOKEN_USER.USER_KEY);
+    return userStr ? JSON.parse(userStr) : null;
+  }
+
+  clearAll(): void {
+    localStorage.removeItem(TOKEN_ROUTES.TOKEN_USER.TOKEN_KEY);
+    localStorage.removeItem(TOKEN_ROUTES.TOKEN_USER.REFRESH_TOKEN_KEY);
+    localStorage.removeItem(TOKEN_ROUTES.TOKEN_USER.USER_KEY);
   }
 
   getAccessToken(): string | null {
-    return localStorage.getItem('accessToken');
+    return localStorage.getItem(TOKEN_ROUTES.TOKEN_USER.TOKEN_KEY);
   }
 
   getRefreshToken(): string | null {
-    return localStorage.getItem('refreshToken');
+    return localStorage.getItem(TOKEN_ROUTES.TOKEN_USER.REFRESH_TOKEN_KEY);
   }
 
   isAuthenticated(): boolean {
-    return !!this.getAccessToken();
+    return !!this.getAccessToken() && !!this.getUserInfo();
   }
 }
