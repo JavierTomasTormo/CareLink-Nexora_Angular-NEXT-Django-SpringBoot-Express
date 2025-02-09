a6864a8cf9358b1266c7e030a436792f6533cb5f
JavierTomasTormo <106741000+JavierTomasTormo@users.noreply.github.com>
2025-02-09 20:38:04 +0100

    AuthListerner --> Next --> Comprueba
    comprueba correctamente que esta loggeado alguien y

--- fullstack/next/src/components/layout/Header.tsx
+++ fullstack/next/src/components/layout/Header.tsx
@@ -1,13 +1,23 @@
 'use client';
 
 import Link from 'next/link';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import styles from '../../styles/layout/Header.module.css';
 import { SHARED_ROUTES } from '@/store/Constants';
-import { isAuthenticated, logout } from '../../utils/auth';
+import { isAuthenticated, logout, authListener } from '../../utils/auth';
 
 const Header: React.FC = () => {
   const [menuOpen, setMenuOpen] = useState(false);
+  const [isAuth, setIsAuth] = useState(false);
+
+  useEffect(() => {
+    setIsAuth(isAuthenticated());
+    const cleanup = authListener((newAuthState) => {
+      setIsAuth(newAuthState);
+    });
+    return cleanup;
+  }, []);
+
 
   const toggleMenu = () => {
     setMenuOpen(!menuOpen);
@@ -43,18 +53,29 @@ const Header: React.FC = () => {
                 Menús
               </Link>
             </li>
-            {!isAuthenticated() ? (
-                <li className={styles.navItem}>
-                  <a href={SHARED_ROUTES.ANGULAR.AUTH.LOGIN} className={`${styles.navLink} ${styles.authButton}`}>
-                    Login
-                  </a>
-                </li>
-              ) : (
-                <li className={styles.navItem}>
-                  <button onClick={logout} className={styles.navLink}>
-                    Logout
-                  </button>
-                </li>
+            {!isAuth ? (
+              <li className={styles.navItem}>
+                <Link
+                  href={SHARED_ROUTES.ANGULAR.AUTH.LOGIN}
+                  className={`${styles.navLink} ${styles.authButton}`}
+                >
+                  Login
+                </Link>
+              </li>
+            ) : (
+              <li className={styles.navItem}>
+                <a
+                  href="#"
+                  onClick={(e) => {
+                    e.preventDefault();
+                    logout();
+                    setIsAuth(false); 
+                  }}
+                  className={`${styles.navLink} ${styles.authButton}`}
+                >
+                  Logout
+                </a>
+              </li>
             )}
           </ul>
         </nav>
@@ -65,57 +86,3 @@ const Header: React.FC = () => {
 
 export default Header;
 
-
-
-// 'use client';
-
-// import Link from 'next/link';
-// import { useState } from 'react';
-// import styles from '../../styles/layout/Header.module.css';
-
-// const Header: React.FC = () => {
-//   const [menuOpen, setMenuOpen] = useState(false);
-  
-
-//   const toggleMenu = () => {
-//     setMenuOpen(!menuOpen);
-//   };
-
-//   return (
-//     <header className={styles.header}>
-//       <div className={styles.container}>
-//         <div className={styles.logo}>
-//           <Link href="/home">
-//             <img src="/Logo_VitalNest.png" alt="VitalNest Logo" />
-//           </Link>
-//         </div>
-//         <button className={styles.hamburger} onClick={toggleMenu} aria-label="Toggle menu">
-//           <span className={styles.bar}></span>
-//           <span className={styles.bar}></span>
-//           <span className={styles.bar}></span>
-//         </button>
-//         <nav className={`${styles.nav} ${menuOpen ? styles.navOpen : ''}`}>
-//           <ul className={styles.navList}>
-//             <li className={styles.navItem}>
-//               <Link href="/home" className={styles.navLink}>
-//                 Página Principal
-//               </Link>
-//             </li>
-//             <li className={styles.navItem}>
-//               <Link href="/shop?activity_type=1" className={styles.navLink}>
-//                 Actividades
-//               </Link>
-//             </li>
-//             <li className={styles.navItem}>
-//               <Link href="/meals?type_meal=1" className={styles.navLink}>
-//                 Menús
-//               </Link>
-//             </li>
-//           </ul>
-//         </nav>
-//       </div>
-//     </header>
-//   );
-// };
-
-// export default Header;
