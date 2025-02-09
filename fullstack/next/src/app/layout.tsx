1a2642eafb14807e8d5b33866bc9c7c9defe0fda
JavierTomasTormo <106741000+JavierTomasTormo@users.noreply.github.com>
2025-02-09 19:35:57 +0100

    Providers--> Next --> Provide Auth Next

--- fullstack/next/src/app/layout.tsx
+++ fullstack/next/src/app/layout.tsx
@@ -3,15 +3,18 @@ import Header from "@/components/layout/Header";
 import Footer from "@/components/layout/Footer";
 import "../styles/global.css";
 import styles from "../styles/layout/Layout.module.css";
+import AuthProvider from "@/providers/AuthProvider";
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="es">
       <body>
         <Providers>
-          <Header />
-          <main className={styles.main}>{children}</main>
-          <Footer />
+          <AuthProvider>
+              <Header />
+                <main className={styles.main}>{children}</main>
+              <Footer />
+          </AuthProvider>
         </Providers>
       </body>
     </html>
