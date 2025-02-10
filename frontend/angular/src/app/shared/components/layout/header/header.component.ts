93cdbb4d78455cd34d2c3f1190c73a486ed3c8fa
JavierTomasTormo <106741000+JavierTomasTormo@users.noreply.github.com>
2025-02-10 11:26:57 +0100

    ToggleMenu--> Angular --> Profile

--- frontend/angular/src/app/shared/components/layout/header/header.component.ts
+++ frontend/angular/src/app/shared/components/layout/header/header.component.ts
@@ -4,6 +4,7 @@ import { CommonModule } from '@angular/common';
 import { TokenService } from '../../../../core/services/token/token.service';
 import { UserService } from '../../../../core/services/auth/user.service';
 import { SHARED_ROUTES } from '../../../../core/constants/shared.routes';
+import { User } from '../../../../core/models/Users/user.model';
 
 @Component({
     selector: 'app-header',
@@ -15,12 +16,29 @@ import { SHARED_ROUTES } from '../../../../core/constants/shared.routes';
 export class HeaderComponent {
     menuOpen = false;
     routes = SHARED_ROUTES;
+    profileMenuOpen = false;
+    user!: User;
     
     constructor(
         public tokenService: TokenService,
         private userService: UserService,
     ) {}
     
+    
+    ngOnInit() {
+        if (this.tokenService.isAuthenticated()) {
+            const userInfo = this.tokenService.getUserInfo();
+            if (userInfo) {
+                this.user = userInfo;
+            }
+        }
+    }
+
+    toggleProfileMenu() {
+        // console.log('toggleProfileMenu called', this.user);
+        this.profileMenuOpen = !this.profileMenuOpen;
+    }
+
     toggleMenu() {
         this.menuOpen = !this.menuOpen;
     }
