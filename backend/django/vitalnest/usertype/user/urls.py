6de6c68de3949ece8e98bb1653ac8effae9b5ea5
JavierTomasTormo <106741000+JavierTomasTormo@users.noreply.github.com>
2025-02-11 18:55:27 +0100

    User & UserPatient --> Django --> Update No Password
    no se necesita la password para hacer update

--- backend/django/vitalnest/usertype/user/urls.py
+++ backend/django/vitalnest/usertype/user/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import UserList
+from .views import UserList, UserDetail
 
 urlpatterns = [
     path('users/tutor/', UserList.as_view(), name='user-list'),
+    path('users/tutor/<int:pk>/', UserDetail.as_view(), name='user-detail'),
 ]
