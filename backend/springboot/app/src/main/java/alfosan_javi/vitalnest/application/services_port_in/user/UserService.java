6d6c8c781618afc6cd931d13bc34be4a056d4275
Llorenç Alfonso Sanchis <163330679+alfosan@users.noreply.github.com>
2025-02-11 12:43:42 +0100

    User Deleted --> SpringBoot --> Dto, Service, Repo

--- backend/springboot/app/src/main/java/alfosan_javi/vitalnest/application/services_port_in/user/UserService.java
+++
@@ -1,6 +0,0 @@
-package alfosan_javi.vitalnest.application.services_port_in.user;
-
-public interface UserService {
-    boolean existsByEmail(String email);
-    Long getUserIdByEmail(String email);
-}
--- backend/springboot/app/src/main/java/alfosan_javi/vitalnest/domain/repos_port_out/user/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package alfosan_javi.vitalnest.domain.repos_port_out.user;
-
-import alfosan_javi.vitalnest.domain.models.User;
-import java.util.Optional;
-
-public interface UserRepository {
-    Optional<User> findByEmail(String email);
-}
