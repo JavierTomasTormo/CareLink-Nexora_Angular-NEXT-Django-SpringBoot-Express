776c44e41119fa7b63ce0a8385773a53e27f4289
Llorenç Alfonso Sanchis <163330679+alfosan@users.noreply.github.com>
2025-02-11 12:42:31 +0100

    Config and access_token --> SpringBoot && DJango --> Config jwt and Access Token

--- backend/springboot/app/build.gradle.kts
+++ backend/springboot/app/build.gradle.kts
@@ -22,7 +22,7 @@ dependencies {
     // Starter para Spring Security
     implementation("org.springframework.boot:spring-boot-starter-security")
 
-    // JWT
+    // Dependencias de JWT (Para manejar JWT)
     implementation("io.jsonwebtoken:jjwt-api:0.11.5")
     runtimeOnly("io.jsonwebtoken:jjwt-impl:0.11.5")
     runtimeOnly("io.jsonwebtoken:jjwt-jackson:0.11.5")
