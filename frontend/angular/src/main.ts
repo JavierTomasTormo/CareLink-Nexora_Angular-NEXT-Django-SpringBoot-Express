02697e222eb18560893055ef2197b9869875c34c
JavierTomasTormo <106741000+JavierTomasTormo@users.noreply.github.com>
2025-02-13 09:06:36 +0100

    Chart.js --> Angular --> Dependecies

--- frontend/angular/src/main.ts
+++ frontend/angular/src/main.ts
@@ -1,6 +1,20 @@
 import { bootstrapApplication } from '@angular/platform-browser';
+import { provideHttpClient } from '@angular/common/http';
 import { AppComponent } from './app/app.component';
-import { appConfig } from './app/app.config';
+import { provideRouter } from '@angular/router';
+import { routes } from './app/app.routes';
+import { importProvidersFrom } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 
-bootstrapApplication(AppComponent, appConfig)
-  .catch(err => console.error(err));
+bootstrapApplication(AppComponent, {
+  providers: [
+    provideRouter(routes),
+    importProvidersFrom(HttpClientModule)
+  ]
+}).catch(err => console.error(err));
+// import { bootstrapApplication } from '@angular/platform-browser';
+// import { AppComponent } from './app/app.component';
+// import { appConfig } from './app/app.config';
+
+// bootstrapApplication(AppComponent, appConfig)
+//   .catch(err => console.error(err));
