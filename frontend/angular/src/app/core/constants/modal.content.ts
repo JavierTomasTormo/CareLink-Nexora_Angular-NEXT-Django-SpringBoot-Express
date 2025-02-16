4fedd0140a8dcd1bee11f922aa17dcf9fdcc96ac
JavierTomasTormo <106741000+JavierTomasTormo@users.noreply.github.com>
2025-02-16 18:34:55 +0100

    Modals Content --> Angular --> Add Constants

--- frontend/angular/src/app/core/constants/modal.content.ts
+++ frontend/angular/src/app/core/constants/modal.content.ts
@@ -61,3 +61,63 @@
         'Plantas (hiedra venenosa, roble venenoso, etc.)'
         ]
     };
+
+
+
+
+        export type DifficultiesCategories = {
+            'Movilidad': string[];
+            'Comunicación': string[];
+            'Cognitivas': string[];
+            'Sensoriales': string[];
+            'Cuidado Personal': string[];
+            'Social': string[];
+        }
+        
+        export const DEFAULT_DIFFICULTY_CATEGORY: keyof DifficultiesCategories = 'Movilidad';
+        
+        export const DIFFICULTIES_CATEGORIES: DifficultiesCategories = {
+            'Movilidad': [
+            'Dificultad para caminar',
+            'Problemas de equilibrio',
+            'Limitación en extremidades superiores',
+            'Limitación en extremidades inferiores',
+            'Necesidad de ayudas técnicas',
+            'Dificultad para subir/bajar escaleras'
+            ],
+            'Comunicación': [
+            'Dificultad en el habla',
+            'Problemas de audición',
+            'Dificultad para escribir',
+            'Dificultad para comprender',
+            'Tartamudeo'
+            ],
+            'Cognitivas': [
+            'Pérdida de memoria',
+            'Dificultad de concentración',
+            'Desorientación',
+            'Dificultad para tomar decisiones',
+            'Problemas de aprendizaje'
+            ],
+            'Sensoriales': [
+            'Deficiencia visual',
+            'Sensibilidad a la luz',
+            'Sensibilidad al ruido',
+            'Problemas de equilibrio',
+            'Dificultad con la profundidad'
+            ],
+            'Cuidado Personal': [
+            'Dificultad para vestirse',
+            'Dificultad para alimentarse',
+            'Dificultad para el aseo personal',
+            'Necesidad de supervisión',
+            'Dificultad para tomar medicamentos'
+            ],
+            'Social': [
+            'Dificultad para socializar',
+            'Ansiedad social',
+            'Problemas de conducta',
+            'Dificultad para mantener relaciones',
+            'Aislamiento'
+            ]
+        };
