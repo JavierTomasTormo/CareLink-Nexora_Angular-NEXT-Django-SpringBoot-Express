d42ee2921d64d6a565160d3c37632c9f3dd6714d
JavierTomasTormo <106741000+JavierTomasTormo@users.noreply.github.com>
2025-02-21 17:08:08 +0100

    Fix --> Angular --> Bookings View

--- frontend/angular/src/app/shared/components/profile/bookings-view/bookings-view.component.ts
+++ frontend/angular/src/app/shared/components/profile/bookings-view/bookings-view.component.ts
@@ -3,7 +3,9 @@ import { CommonModule } from '@angular/common';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { BookingsService } from '../../../../core/services/bookings/bookings.service';
 import { ActivityService } from '../../../../core/services/activities/activity.service';
-import { UserPatientService } from '../../../../core/services/users/user-patient-2.service';
+import { UserPatientService } from '../../../../core/services/users/user-patient.service';
+import { UserPatient } from '../../../../core/models/Users/user-patient.model';
+import { NgbModal, NgbModalRef } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-bookings-view',
@@ -16,21 +18,37 @@ export class BookingsViewComponent implements OnInit {
   bookings: any[] = [];
   isLoading = true;
   errorMessage = '';
+  selectedActivity: any; 
+  selectedPatient: UserPatient | undefined;
+  modalRef?: NgbModalRef;
+
 
   constructor(
     private bookingsService: BookingsService,
     private activityService: ActivityService,
-    private userPatientService: UserPatientService
+    private userPatientService: UserPatientService,
+    private modalService: NgbModal
   ) {}
 
   ngOnInit(): void {
     this.fetchBookings();
   }
 
+  showActivityDetails(modal: any, activity: any): void {
+    this.selectedActivity = activity;
+    this.modalRef = this.modalService.open(modal);
+  }
+
+  showPatientDetails(modal: any, patient: UserPatient): void {
+    this.selectedPatient = patient;
+    this.modalRef = this.modalService.open(modal);
+  }
+  
+
   fetchBookings(): void {
     this.bookingsService.getBookings().subscribe({
       next: (data) => {
-        console.log('Bookings data:', data); // Log de depuración
+        console.log('Bookings data:', data); 
         this.bookings = data;
         this.loadAdditionalData();
         this.isLoading = false;
@@ -45,10 +63,10 @@ export class BookingsViewComponent implements OnInit {
 
   loadAdditionalData(): void {
     this.bookings.forEach(booking => {
-      console.log('Fetching activity for booking:', booking.idActivity); // Log de depuración
+      console.log('Fetching activity for booking:', booking.idActivity); 
       this.activityService.getActivityById(booking.idActivity).subscribe({
         next: (activityData) => {
-          console.log('Activity data for booking', booking.idActivity, ':', activityData); // Log de depuración
+          console.log('Activity data for booking', booking.idActivity, ':', activityData); 
           booking.activity = activityData;
         },
         error: (error) => {
@@ -56,12 +74,12 @@ export class BookingsViewComponent implements OnInit {
         }
       });
 
-      console.log('Fetching patient for booking:', booking.idPatient); // Log de depuración
+      console.log('Fetching patient for booking:', booking.idPatient); 
       this.userPatientService.getUserPatientsByUser(booking.idPatient).subscribe({
         next: (patientData) => {
-          console.log('Patient data for booking', booking.idPatient, ':', patientData); // Log de depuración
+          console.log('Patient data for booking', booking.idPatient, ':', patientData); 
           booking.patient = patientData;
-          console.log('Assigned patient data to booking:', booking.patient); // Log de depuración
+          console.log('Assigned patient data to booking:', booking.patient); 
         },
         error: (error) => {
           console.error('Error fetching patient data:', error);
