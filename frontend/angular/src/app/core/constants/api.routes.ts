3dcaf8fdbbab8f8efccacc400e8f1d2b37782c60
JavierTomasTormo <106741000+JavierTomasTormo@users.noreply.github.com>
2025-02-24 09:55:04 +0100

    Get_All  --> Angular --> Bookings
    hecho de manera correcta sin chapuzas, ejem ejem

--- frontend/angular/src/app/core/constants/api.routes.ts
+++ frontend/angular/src/app/core/constants/api.routes.ts
@@ -12,5 +12,8 @@ export const API_ROUTES = {
         LIST_BY_USER: (userId: number) => `${environment.apiUrl}/users/patient/?id_user=${userId}`,
         CREATE: `${environment.apiUrl}/users/patient/`,
         UPDATE: (patientId: number) => `${environment.apiUrl}/users/patient/${patientId}/`,
-    }
+    },
+    BOOKINGS: {
+        GET_ALL: `http://localhost:8085/inscriptions/user`, //'http://localhost:8085/inscriptions/user';
+    },
 };
--- frontend/angular/src/app/core/services/bookings/bookings.service.ts
+++ frontend/angular/src/app/core/services/bookings/bookings.service.ts
@@ -1,22 +1,21 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { API_ROUTES } from '../../constants/api.routes';
+import { TokenService } from '../token/token.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class BookingsService {
-  private apiUrl = 'http://localhost:8085/inscriptions/user';
 
-  constructor(private http: HttpClient) {}
+  constructor(
+    private http: HttpClient,
+    private tokenService: TokenService
+  ) {}
 
   getBookings(): Observable<any> {
-    const token = localStorage.getItem('accessToken');
-    console.log('Access Token:', token); // Log de depuración
-    const headers = new HttpHeaders({
-      'Authorization': `Bearer ${token}`
-    });
-
-    return this.http.get<any>(this.apiUrl, { headers });
+    const headers = this.tokenService.getAuthorizationHeader() || {}
+    return this.http.get<any>(API_ROUTES.BOOKINGS.GET_ALL, { headers });
   }
 }
