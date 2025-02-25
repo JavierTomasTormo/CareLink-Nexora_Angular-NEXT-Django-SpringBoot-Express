aed6ca5bcc0d0b68aea7b57be1332ba0bd59ea66
JavierTomasTormo <106741000+JavierTomasTormo@users.noreply.github.com>
2025-02-25 11:42:41 +0100

    Optimize --> NEXT --> Services

--- fullstack/next/src/services/inscriptions/inscriptionService.ts
+++ fullstack/next/src/services/inscriptions/inscriptionService.ts
@@ -1,7 +1,20 @@
 import axios from 'axios';
 import { getAccessToken } from '@/utils/auth';
 
-export const createInscription = async (inscriptionData: any) => {
+export interface InscriptionData {
+    idActivity: number;
+    idHour: number;
+    idDay: number;
+    idMonth: number;
+    idYear: number;
+    idPayment: number;
+    idPatient: number;
+    state: string;
+    specialRequest: string;
+    [key: string]: string | number; 
+}
+
+export const createInscription = async (inscriptionData: InscriptionData) => {
     try {
         const token = getAccessToken();
         const response = await axios.post('http://localhost:8085/inscriptions/create', inscriptionData, {
--- fullstack/next/src/services/payments/paymentsService.ts
+++ fullstack/next/src/services/payments/paymentsService.ts
@@ -2,7 +2,11 @@ import axios from 'axios';
 
 interface PaymentIntentResponse {
     client_secret: string;
-    [key: string]: any;
+    id: string;
+    object: string;
+    amount: number;
+    status: string;
+    [key: string]: string | number | boolean | null;
 }
 
 export const createPaymentIntent = async (amount: number): Promise<PaymentIntentResponse> => {
